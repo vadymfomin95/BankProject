7213babab60827ab53d2793297abbe0e1af6f1c4
iuada14s <vadym.fomin@raiffeisen.com>
2025-09-18 14:27:33 +0300

    add eureka server

---
+++ serviceregistry/src/test/java/ua/nure/fomin/serviceregistry/ServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.fomin.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
